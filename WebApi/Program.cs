d6b6a62f1bcf007097119d77c86d78760af9f44d
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 03:45:32 +0000

    update Program.cs: clean up style/format according to .editorconfig

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,22 +1,19 @@
-
 namespace WebApi;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllers();
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        var app = builder.Build();
+        WebApplication app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -24,9 +21,7 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
 
         app.MapControllers();
 
