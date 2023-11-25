788d807b2d6e43ab81a228419d74cb994f2c108f
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 04:39:17 +0000

    update WeatherForecast.cs: define conversion in static class

--- WebApi/WeatherForecast.cs
+++ WebApi/WeatherForecast.cs
@@ -5,5 +5,10 @@ public record class WeatherForecast(
     int      TemperatureC,
     string?  Summary)
 {
-    public int TemperatureF => (int)(TemperatureC / 0.5556) + 32;
+    public int TemperatureF => TemperatureConversions.CelsiusToFahrenheit(TemperatureC);
+}
+
+public static class TemperatureConversions
+{
+    public static int CelsiusToFahrenheit(int degreesCelsius) => (int)(degreesCelsius / 0.5556) + 32;
 }
