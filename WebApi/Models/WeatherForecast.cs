35ea7bf103ce799fff69b3efe1635e719dcaca47
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 16:50:09 +0000

    new factory method for WeatherForecast

--- WebApi/Models/WeatherForecast.cs
+++ WebApi/Models/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using WebApi.Enums;
+using WebApi.Extensions;
 using WebApi.Utilities;
 
 namespace WebApi.Models;
@@ -10,4 +12,10 @@ public record class WeatherForecast(
     public int TemperatureF
         => Conversions
         .CelsiusToFahrenheit(TemperatureC);
+
+    public static WeatherForecast NewRandomWeatherForecast(DateOnly date)
+    {
+        TemperatureDescriptor temperatureDescriptor = RandomExtensions.RandomValue<TemperatureDescriptor>();
+        return new WeatherForecast(date, (int)temperatureDescriptor, temperatureDescriptor.ToString());
+    }
 }
