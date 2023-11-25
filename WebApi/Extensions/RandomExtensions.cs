9efbf21ceef8fc0894488ca8eeef7a005a87cdc6
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 16:45:18 +0000

    use consistent expression style for RandomExtensions

--- WebApi/Extensions/RandomExtensions.cs
+++ WebApi/Extensions/RandomExtensions.cs
@@ -2,6 +2,19 @@ namespace WebApi.Extensions;
 
 public static class RandomExtensions
 {
-    public static T? RandomElement<T>(this List<T?> anyList) => anyList.ElementAt(Random.Shared.Next(0, anyList.Count));
-    public static TEnum RandomValue<TEnum>() where TEnum : struct, Enum => Enum.GetValues<TEnum>().ToList().RandomElement();
+    public static int RandomIndex<T>(this List<T?> anyList)
+        => Random
+        .Shared
+        .Next(0, anyList.Count);
+
+    public static T? RandomElement<T>(this List<T?> anyList)
+        => anyList
+        .ElementAt(anyList.RandomIndex());
+
+    public static TEnum RandomValue<TEnum>()
+        where TEnum : struct, Enum
+        => Enum
+        .GetValues<TEnum>()
+        .ToList()
+        .RandomElement();
 }
