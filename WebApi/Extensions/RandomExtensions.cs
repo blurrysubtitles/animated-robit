fccac7d1f395c26628fe6135c9ea67350eb802aa
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 16:41:16 +0000

    (actually) rename TemperatureDescriptorExtensions to RandomExtensions, and no longer need the using

--- WebApi/Extensions/RandomExtensions.cs
+++ WebApi/Extensions/RandomExtensions.cs
@@ -1,6 +1,6 @@
 namespace WebApi.Extensions;
 
-public static class TemperatureDescriptorExtensions
+public static class RandomExtensions
 {
     public static T? RandomElement<T>(this List<T?> anyList) => anyList.ElementAt(Random.Shared.Next(0, anyList.Count));
     public static TEnum RandomValue<TEnum>() where TEnum : struct, Enum => Enum.GetValues<TEnum>().ToList().RandomElement();
