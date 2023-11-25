9cee2561b3656bbb7e93834d7d87c03b1dbafd4a
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 16:35:58 +0000

    rename TemperatureConversions to just Conversions and put it in a Utilities directory

--- WebApi/Utilities/Conversions.cs
+++ WebApi/Utilities/Conversions.cs
@@ -1,6 +1,6 @@
 namespace WebApi.Extensions;
 
-public static class TemperatureConversions
+public static class Conversions
 {
     public static int CelsiusToFahrenheit(int degreesCelsius)
         => (int)
