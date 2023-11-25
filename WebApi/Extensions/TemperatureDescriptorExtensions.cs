896b143339c0f00f47c18df946ee174adb31c9f1
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 16:33:04 +0000

    update TemperatureDescriptorExtensions: make the list and enum operations generic

--- WebApi/Extensions/TemperatureDescriptorExtensions.cs
+++ WebApi/Extensions/TemperatureDescriptorExtensions.cs
@@ -1,3 +1,9 @@
 using WebApi.Enums;
 
 namespace WebApi.Extensions;
+
+public static class TemperatureDescriptorExtensions
+{
+    public static T? RandomElement<T>(this List<T?> anyList) => anyList.ElementAt(Random.Shared.Next(0, anyList.Count));
+    public static TEnum RandomValue<TEnum>() where TEnum : struct, Enum => Enum.GetValues<TEnum>().ToList().RandomElement();
+}
