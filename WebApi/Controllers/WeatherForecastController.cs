0621742344ae44abcf35c5be43eec91d82453cf9
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 04:45:40 +0000

    update WeatherForecastController: use better styling

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -14,6 +14,9 @@ public class WeatherForecastController(ILogger<WeatherForecastController> logger
     public IEnumerable<WeatherForecast> Get()
         => Enumerable
         .Range(1, 7)
-        .Select(i => new WeatherForecast { Date = DateOnly.FromDateTime(DateTime.Now.AddDays(i)), TemperatureC = Random.Shared.Next(-20, 55), Summary = _summaries[Random.Shared.Next(_summaries.Length)] })
+        .Select(i => new WeatherForecast(
+            Date:         DateOnly.FromDateTime(DateTime.Now.AddDays(i)),
+            TemperatureC: Random.Shared.Next(-20, 55),
+            Summary:      _summaries[Random.Shared.Next(_summaries.Length)]))
         .ToArray();
 }
