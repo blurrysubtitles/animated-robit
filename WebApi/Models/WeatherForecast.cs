6a3f6a98882eab0cca71010e43bf254bc89df507
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 17:46:56 +0000

    create helpful WeatherForecast methods for use and demonstration

--- WebApi/Models/WeatherForecast.cs
+++ WebApi/Models/WeatherForecast.cs
@@ -13,9 +13,24 @@ public record class WeatherForecast(
         => Conversions
         .CelsiusToFahrenheit(TemperatureC);
 
-    public static WeatherForecast Random(DateOnly date)
+    public static WeatherForecast Create(DateOnly date)
     {
         TemperatureDescriptor temperatureDescriptor = RandomExtensions.RandomValue<TemperatureDescriptor>();
         return new WeatherForecast(date, (int)temperatureDescriptor, temperatureDescriptor.ToString());
     }
+
+    public static WeatherForecast OnDay(DateOnly date)
+        => Create(date);
+
+    public static WeatherForecast InDays(int daysFromToday)
+        => OnDay(DateOnlyExtensions.TodayPlus(daysFromToday));
+
+    public static WeatherForecast Today()
+        => InDays(0);
+
+    public static WeatherForecast Tomorrow()
+        => InDays(1);
+
+    public WeatherForecast Next
+        => Create(Date.AddDays(1));
 }
