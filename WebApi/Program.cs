043d5c874bfc8085e46facd213714d0e1cb6aad9
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-25 17:57:04 +0000

    make your own swagger and devcontainer and csproj bookmarks ya hippie (talking to self, likely to regret decision later)

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,7 +9,6 @@ public class Program
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
