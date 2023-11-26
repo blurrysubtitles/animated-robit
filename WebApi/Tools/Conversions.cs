4755bed99fc260b82598cf7fcd0b3b091f8804fb
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-11-26 23:12:41 +0000

    add Enums static class to Tools with Random method to get a random defined value of an enumeration, returning the enum typed value

--- WebApi/Tools/Conversions.cs
+++ WebApi/Tools/Conversions.cs
@@ -1,3 +1,5 @@
+using WebApi.Extensions;
+
 namespace WebApi.Tools;
 
 public static class Conversions
@@ -5,4 +7,13 @@ public static class Conversions
     public static int CelsiusToFahrenheit(int degreesCelsius)
         => (int)
         (degreesCelsius / 0.5556) + 32;
+}
+
+public static class Enums
+{
+    public static TEnum Random<TEnum>() where TEnum : struct, Enum
+        => Enum
+        .GetValues<TEnum>()
+        .ToList()
+        .RandomElement();
 }
